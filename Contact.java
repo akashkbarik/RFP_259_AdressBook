71d89807192ffc6f42e267b0d97d110488ef4c28
akashkbarik <akashkbarik12@gmail.com>
2023-03-03 21:15:39 +0530

    Added: Contact class to the code

---
+++ Contact.java
@@ -0,0 +1,76 @@
+package Assignment.Day9.AddressBook;
+
+public class Contact {
+    String first_name;
+    String last_name;
+    String address;
+    String city;
+    String state;
+    int zip;
+    int phone_no;
+    String Email;
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public int getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(int phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
