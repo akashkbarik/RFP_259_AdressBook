8e7acb45cc3df02295a511c0ef99ceb7c4636aeb
akashkbarik <akashkbarik12@gmail.com>
2023-03-03 21:07:11 +0530

    Added: welcome code

---
+++ AddressBook.java
@@ -0,0 +1,7 @@
+package Assignment.Day9.AddressBook;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("welcome to the AddressBook program.\n..............................");
+    }
+}
